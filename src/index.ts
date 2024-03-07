5d0944a7d9ce3a095b82770f1fe3fde2799e4ffe
marcelldac <dacmarcell@protonmail.com>
2024-03-07 17:00:32 -0300

    style: add information comments

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,13 @@ app.use(cors());
 
 app.use(express.json());
 
+//create , read , update , delete
+//CRUD
+// catch toda vez com status 500(Internal server error)
+
+// delete => 204 (no content)
+
+
 app.listen(PORT, function () {
   console.log(`Servidor Online! Porta: ${PORT}`);
 });

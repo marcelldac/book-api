e567177127f60699d4c6291d03b19e49f9e0e340
dacmarcell <devmarcell@protonmail.com>
2024-03-14 14:16:16 -0300

    refactor: use book service

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,65 +1,26 @@
 import { Router } from "express";
-import prismaClient from "../../prisma/prisma-client";
+import bookService from "../services/book-service";
 
 const routes = Router();
 
 routes.get("/books", async (request, response) => {
-  try {
-    const books = await prismaClient.book.findMany();
-    return response.status(200).json(books);
-  } catch (error) {
-    return response.status(500).json({ message: error, error: true });
-  }
+  const books = await bookService.read();
+  return response.json(books);
 });
 
-//Create Books
 routes.post("/books", async (request, response) => {
-  try {
-    const { name, synopsis, value, genders } = request.body;
-    const newBook = await prismaClient.book.create({
-      data: {
-        name,
-        synopsis,
-        value,
-        genders,
-      },
-    });
-    //201 = created
-    return response.status(201).json(newBook);
-  } catch (error) {
-    return response.status(500).json({ message: error, error: true });
-  }
+  const newBook = await bookService.create(request.body);
+  return response.status(201).json(newBook);
 });
 
 routes.put("/books/:id", async (request, response) => {
-  try {
-    const { id } = request.params;
-    const { name, synopsis, value, genders } = request.body;
-
-    const updateBook = await prismaClient.book.update({
-      data: { name, synopsis, value, genders },
-      where: { id },
-    });
-
-    return response.status(200).json(updateBook);
-  } catch (error) {
-    return response.status(500).json({ message: error, error: true });
-  }
+  const updatedBook = await bookService.update(request.body, request.params.id);
+  return response.json(updatedBook);
 });
 
 routes.delete("/books/:id", async (request, response) => {
-  try {
-    const { id } = request.params;
-
-    await prismaClient.book.delete({
-      where: { id },
-    });
-
-    //204 => no content (sem conteúdo)
-    return response.sendStatus(204);
-  } catch (error) {
-    return response.status(500).json({ message: error, error: true });
-  }
+  const removedBook = await bookService.remove(request.params.id);
+  return response.json(removedBook);
 });
 
 export default routes;
